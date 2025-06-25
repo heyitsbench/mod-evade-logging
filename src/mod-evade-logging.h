5af4317d227abee07f972ff794a8add49f57e05e
Stefano Borzì <stefanoborzi32@gmail.com>
2025-06-25 13:26:14 +0200

    refactor: remove breaks after return

--- src/mod-evade-logging.h
+++ src/mod-evade-logging.h
@@ -13,31 +13,22 @@ std::string GetUnitTypeText(TypeID typeID)
     {
     case TYPEID_OBJECT:
         return "TYPEID_OBJECT";
-        break;
     case TYPEID_ITEM:
         return "TYPEID_ITEM";
-        break;
     case TYPEID_CONTAINER:
         return "TYPEID_CONTAINER";
-        break;
     case TYPEID_UNIT:
         return "TYPEID_UNIT";
-        break;
     case TYPEID_PLAYER:
         return "TYPEID_PLAYER";
-        break;
     case TYPEID_GAMEOBJECT:
         return "TYPEID_GAMEOBJECT";
-        break;
     case TYPEID_DYNAMICOBJECT:
         return "TYPEID_DYNAMICOBJECT";
-        break;
     case TYPEID_CORPSE:
         return "TYPEID_CORPSE";
-        break;
     default:
         return "TYPEID_UNKNOWN";
-        break;
     }
 }
 
@@ -47,22 +38,16 @@ std::string GetEvadeReasonText(uint8 evadeReason)
     {
     case 0:
         return "EVADE_REASON_NO_HOSTILES";
-        break;
     case 1:
         return "EVADE_REASON_BOUNDARY";
-        break;
     case 2:
         return "EVADE_REASON_SEQUENCE_BREAK";
-        break;
     case 3:
         return "EVADE_REASON_NO_PATH";
-        break;
     case 4:
         return "EVADE_REASON_OTHER";
-        break;
     default:
         return "EVADE_REASON_UNKNOWN";
-        break;
     }
 }
 
